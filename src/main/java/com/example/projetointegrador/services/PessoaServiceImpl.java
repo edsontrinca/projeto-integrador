0edc9deda823340c2c9d1411e463a6dd63276135
edsontrinca <edsonricardotrinca@gmail.com>
2022-11-01 19:43:55 -0300

    Atualização dos arquivos do projeto.

---
+++ src/main/java/com/example/projetointegrador/services/PessoaServiceImpl.java
@@ -0,0 +1,27 @@
+package com.example.projetointegrador.services;
+
+import com.example.projetointegrador.dto.PessoaDTO;
+import com.example.projetointegrador.models.Pessoa;
+import com.example.projetointegrador.repositories.PessoaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PessoaServiceImpl {
+    final PessoaRepository pessoaRepository;//realiza as consutas e incerções no banco
+
+    public PessoaServiceImpl(PessoaRepository pessoaRepository) {
+        this.pessoaRepository = pessoaRepository;
+    }
+
+    public Pessoa salvarPessoa(Pessoa pessoa){
+        Pessoa pessoa1 = pessoaRepository.save(pessoa);
+        return pessoa1;
+    }
+
+    public List<Pessoa> buscarPessoa(){
+        return pessoaRepository.findAll();
+    }
+
+}
